7a277cea3dd26e27efde8fb455a98547e55810e7
astyd256 <astyd637@gmail.com>
2022-05-13 13:27:54 +0700

    Lab 11 done

---
+++ lab 11/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace lab_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chi_label = new System.Windows.Forms.Label();
+            this.variance_label = new System.Windows.Forms.Label();
+            this.expectation_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Right;
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(247, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(553, 450);
+            this.chart1.SuppressExceptions = true;
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mean";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0.2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Variance";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Sample size";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(113, 105);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(103, 20);
+            this.textBox6.TabIndex = 14;
+            this.textBox6.Text = "10";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chi_label);
+            this.panel2.Controls.Add(this.variance_label);
+            this.panel2.Controls.Add(this.expectation_label);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 333);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(247, 117);
+            this.panel2.TabIndex = 15;
+            // 
+            // chi_label
+            // 
+            this.chi_label.AutoSize = true;
+            this.chi_label.Location = new System.Drawing.Point(13, 69);
+            this.chi_label.Name = "chi_label";
+            this.chi_label.Size = new System.Drawing.Size(35, 13);
+            this.chi_label.TabIndex = 2;
+            this.chi_label.Text = "label7";
+            // 
+            // variance_label
+            // 
+            this.variance_label.AutoSize = true;
+            this.variance_label.Location = new System.Drawing.Point(13, 47);
+            this.variance_label.Name = "variance_label";
+            this.variance_label.Size = new System.Drawing.Size(35, 13);
+            this.variance_label.TabIndex = 1;
+            this.variance_label.Text = "label7";
+            // 
+            // expectation_label
+            // 
+            this.expectation_label.AutoSize = true;
+            this.expectation_label.Location = new System.Drawing.Point(13, 24);
+            this.expectation_label.Name = "expectation_label";
+            this.expectation_label.Size = new System.Drawing.Size(35, 13);
+            this.expectation_label.TabIndex = 0;
+            this.expectation_label.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label chi_label;
+        private System.Windows.Forms.Label variance_label;
+        private System.Windows.Forms.Label expectation_label;
+    }
+}
+
---
+++ lab 11/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const double CHI_VALUE = 9.488;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            double math_expectation = Convert.ToDouble(textBox1.Text);
+            double math_variance = Convert.ToDouble(textBox2.Text);
+
+            int sample_size = Convert.ToInt32(textBox6.Text);
+            var random = new Random();
+            
+            double[] Stndrt_distr = new double[sample_size];
+            double[] Gauss_distr = new double[sample_size];
+
+            Stndrt_distr[0] = Math.Sqrt(-2 * Math.Log(random.NextDouble())) * Math.Sin(2 * Math.PI * random.NextDouble());
+            Gauss_distr[0] = Math.Sqrt(math_variance) * Stndrt_distr[0] + math_expectation;
+
+            double min = Gauss_distr[0];
+            double max = Gauss_distr[0];
+            for (int i = 1; i < sample_size; i++)
+            {
+                Stndrt_distr[i] = Math.Sqrt(-2 * Math.Log(random.NextDouble())) * Math.Sin(2 * Math.PI * random.NextDouble());
+                Gauss_distr[i] = Math.Sqrt(math_variance) * Stndrt_distr[i] + math_expectation;
+                if (Gauss_distr[i] > max) max = Gauss_distr[i];
+                if (Gauss_distr[i] < min) min = Gauss_distr[i];
+            }
+
+            int k = (int)Math.Floor(Math.Log(sample_size, 2)); //number of intervals
+
+            int[] statistics = new int[k];
+            double intervals = Math.Abs(max - min) / k;
+
+            for (int i = 0; i < sample_size; i++) //counting results
+            {
+                for (int j = 1; j < k; j++) // deciding in which interval to add
+                    if (Gauss_distr[i] < min + intervals * (j + 1)) { statistics[j]++; break; }
+            }
+
+            double emperic_expectation = 0;
+            double emperic_variance = 0;
+
+            /*getting emperic values*/
+            for (int i = 0; i < sample_size; i++) emperic_expectation += Gauss_distr[i];
+            emperic_expectation /= sample_size;
+            for (int i = 0; i < sample_size; i++) emperic_variance += Gauss_distr[i] * Gauss_distr[i];
+            emperic_variance = emperic_variance / sample_size - emperic_expectation * emperic_expectation;
+
+            ///*counting errors*/
+            double expectation_error = Math.Abs(emperic_expectation - math_expectation) / Math.Abs(math_expectation);
+            double variance_error = Math.Abs(emperic_variance - math_variance) / Math.Abs(math_variance);
+
+            /*outputing result*/
+            for (int i = 0; i < k; i++) chart1.Series[0].Points.AddXY($"({Math.Round(min + intervals * (i), 1)}; " +
+                $"{Math.Round(min + intervals * (i + 1), 1)}] ", (double)statistics[i] / sample_size);
+            expectation_label.Text = $"Average: {Math.Round(emperic_expectation, 2)} (error = {Math.Round(expectation_error * 100)}%)";
+            variance_label.Text = $"Variance: {Math.Round(emperic_variance, 2)} (error = {Math.Round(variance_error * 100)}%)";
+
+            double cur_chi = 0;
+            double pi = 0;
+            double x = 0;
+            for (int i = 0; i < k; i++)
+            {
+
+                //x = min + intervals * (i + 1) + min + intervals * i / 2;
+                //pi = (min + intervals * (i + 1) - (min + intervals * i))
+                //    * 1 / Math.Sqrt(2 * math_variance * Math.PI)
+                //    * Math.Exp(Math.Pow(x - math_expectation, 2) / (2 * math_variance));
+
+                pi = (min + intervals * (i + 1) - (min + intervals * i))
+                    * (double)statistics[i] / sample_size
+                    * (min + intervals * (i + 1) + min + intervals * i) / 2;
+
+                if (pi != 0) cur_chi += Math.Pow(statistics[i], 2) / (sample_size * pi);
+            } 
+            cur_chi -= sample_size;
+            chi_label.Text = $"Chi-squared: {Math.Round(cur_chi, 2)} > {CHI_VALUE} is {(cur_chi > CHI_VALUE ? "TRUE" : "FALSE")}";
+        }
+    }
+}
