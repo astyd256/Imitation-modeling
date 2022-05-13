c76b14a906522cb204d44792b1ccb1aa55f2093d
astyd256 <astyd637@gmail.com>
2022-05-13 07:19:27 +0700

    lab 10 is done

---
+++ lab 10/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace lab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.Location = new System.Drawing.Point(215, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.Size = new System.Drawing.Size(585, 450);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Team count";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Max is 16 team";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Team Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Lambda";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Wins";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Loses";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Draws";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Score";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ lab 10/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<int, string> names = new Dictionary<int, string>()
+        {
+            { 1, "Real Madrid"},
+            { 2, "Unstoppable"},
+            { 3, "Alliance"},
+            { 4, "Collective"},
+            { 5, "Avengers"},
+            { 6, "Phenomenon"},
+            { 7, "Rumble"},
+            { 8, "Obliterators"},
+            { 9, "Lightning"},
+            { 10, "Assassins"},
+            { 11, "Outlaws"},
+            { 12, "End Game"},
+            { 13, "Rebels"},
+            { 14, "Force"},
+            { 15, "All-Stars"},
+            { 16, "Hustle"}
+        };
+        private struct Team
+        {
+            public string name;
+            public int wins;
+            public int loses;
+            public int draw;
+            public int lambda;
+        }
+        // POINTS SYSTEM: WIN = 3PNT, DRAW = 1PNT, LOSS = 0PNT.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var random = new Random();
+            int m = Convert.ToInt32(textBox1.Text);
+            if (m > 0 && m < 17)
+            {
+                Team[] team = new Team[m];
+                for (int i = 0; i < m; i++)
+                {
+                    team[i].name = names[i + 1];
+                    team[i].lambda = random.Next(5);
+                    team[i].wins = 0;
+                    team[i].loses = 0;
+                    team[i].draw = 0;
+                }
+                double S;
+                int GOAL1;
+                int GOAL2;
+
+                for (int j = 0; j < m; j++)
+                {
+                    for(int i = 0; i < m; i++)
+                    {
+                        if (i != j)
+                        {
+                            S = 0;
+                            GOAL1 = 0;
+                            for (; S >= -team[j].lambda; S += Math.Log(random.NextDouble())) GOAL1++;
+                            S = 0;
+                            GOAL2 = 0;
+                            for (; S >= -team[i].lambda; S += Math.Log(random.NextDouble())) GOAL2++;
+                            if (GOAL1 > GOAL2)
+                            {
+                                team[j].wins++;
+                                team[i].loses++;
+                            }
+                            else if (GOAL1 < GOAL2)
+                            {
+                                team[j].loses++;
+                                team[i].wins++;
+                            }
+                            else
+                            {
+                                team[i].draw++;
+                                team[j].draw++;
+                            }
+                        }
+                    }
+                }
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < m; i++)
+                {
+                    dataGridView1.Rows.Add(1);
+                    dataGridView1.Rows[i].Cells[0].Value = team[i].name;
+                    dataGridView1.Rows[i].Cells[1].Value = team[i].lambda;
+                    dataGridView1.Rows[i].Cells[2].Value = team[i].wins;
+                    dataGridView1.Rows[i].Cells[3].Value = team[i].loses;
+                    dataGridView1.Rows[i].Cells[4].Value = team[i].draw;
+                    dataGridView1.Rows[i].Cells[5].Value = team[i].wins * 3 + team[i].draw;
+                    dataGridView1.Sort(dataGridView1.Columns[5], ListSortDirection.Descending);
+                }
+            }
+        }
+    }
+}
